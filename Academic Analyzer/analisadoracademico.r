626d0ba8f40502475d6420373c7d9fb78320e0e6
Laila Mota <lailamota.238@gmail.com>
2024-12-08 18:33:47 -0300

    Relatório com a classificação dos alunos e gráfico de distribuição das notas, destacando os alunos com notas acima da média.

--- Academic Analyzer/analisadoracademico.r
+++ Academic Analyzer/analisadoracademico.r
@@ -1,3 +1,9 @@
 notas <- c(7.5, 8.2, 6.3, 5.9, 9.1, 7.0, 8.4, 6.8, 7.3, 5.5)
 
 average_class <- mean(grades)
+above_average <- grades[grades > average_class] 
+rating <- ifelse(grades >= 8, "Excellent",
+ifelse(grades >= 6, "Good",
+ifelse(grades >= 4, "Regular", "Insufficient"))) 
+report <- data.frame(Student = 1:length(grades), Grade = grades, Rating = rating) 
+hist(grades, main = "Grade Distribution", xlab = "Grades", col = "lightblue", border = "black") 
