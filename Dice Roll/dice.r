21a4d6dbb557542206cd09978884000964da95d0
Laila Mota <lailamota.238@gmail.com>
2024-12-08 19:04:02 -0300

    Displays probabilities and generates bar chart

--- Dice Roll/dice.r
+++ Dice Roll/dice.r
@@ -4,3 +4,6 @@ frequency <- table(rolls)
 theoretical_probability <- rep(1/6, 6)
 real_probability <- frequency / n
 print(frequency)
+cat("Theoretical Probability: ", theoretical_probability, "\n")
+cat("Real Probability: ", real_probability, "\n")
+barplot(frequency, main = "Roll Frequency", xlab = "Die Faces", ylab = "Frequency", col = "lightblue")
