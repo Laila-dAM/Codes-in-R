a5fdaa6987902059df00dfeb6fbfe11863ba604d
Laila Mota <lailamota.238@gmail.com>
2024-12-09 14:34:08 -0300

    Calculate number of words, phrases, unique words and frequencies

--- Text Analyzer/TextAn.r
+++ Text Analyzer/TextAn.r
@@ -1,3 +1,8 @@
 text <- tolower(readLines())
 words <- unlist(strsplit(text, "\\s+"))
 words <- words[words != ""]
+
+num_words <- length(words)
+num_sentences <- length(unlist(strsplit(text, "[.!?]")))
+unique_words <- length(unique(words))
+word_freq <- table(words)
